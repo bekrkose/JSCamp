824e570d1498a3a27df5f0e7a40b2bbf22993de6
bekrkose <bekir_kose@outlook.com>
2021-10-19 00:09:17 +0300

    JS Kamp İlk Gün Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,28 @@
+
+let dolarBugun=9.30
+
+let dolarDun=9.20
+
+dolarDun="9.20"
+
+{
+    let dolarDun=9.10
+}
+
+console.log(dolarDun)
+
+const euroDun=11.2
+euroDun=11
+
+console.log(euroDun)
+
+Auto Rename Tag 
+
+Bracket Pair Colorizer 2
+
+EditorConfig for VS ConvolverNode
+ES7 React/Redux/
+
+Material Icon Theme
+
+
